a17ad6366d8a682894a9096c08ceb6867c6fd0c8
JoseMFT <jomaft03@gmail.com>
2022-09-24 19:06:26 +0100

    Update MovimientoJugador.cs
    Agregados los cambios para que los controles se inviertan durante 5 segundos cuando el jugador entra en contacto con un bloque que invierte los controles.
    A ARREGLAR:
    - Sigo sin tener los modelos en este PC.

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -2,18 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoJugador : MonoBehaviour
-{
+public class MovimientoJugador : MonoBehaviour {
     public float movimientoEjeX;
     public float movimientoEjeY;
     public float movimientoEjeZ;
-
     public float velMovimiento = 2.3f;
-
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -24,4 +17,28 @@ public class MovimientoJugador : MonoBehaviour
 
         transform.Translate(movimientoEjeX, movimientoEjeY, movimientoEjeZ);
     }
+
+    private class MovimientoInvertido : MonoBehaviour {
+        bool choqueConInvertido = false;
+        double tiempoInvertido = 5d;
+
+        private void OnCollisionEnter(Collision choque) {
+
+            if (choqueConInvertido == true) {
+                movimientoEjeX = - Input.GetAxis("Horizontal") * Time.deltaTime * velMovimiento;
+                movimientoEjeZ = - Input.GetAxis("Vertical") * Time.deltaTime * velMovimiento;
+                timepoInvertido = tiempoInvertido - Time.deltaTime;
+
+                if (tiempoInvertido < 0.0d) {
+                    choqueConInvertido = false;
+                    tiempoInvertido = 5.0d;
+                }
+            }
+
+            if (choque.gameObject.tag == /*"a rellenar"*/) {
+                choqueConInvertido = true;
+            }
+    }
+
+}
 }
